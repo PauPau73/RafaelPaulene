3cf52601092b4ba3829a36bae98a4f56036292cd
Paulene Rafael <paupau73@gmail.com>
2025-09-18 16:26:26 +0800

    First Commit

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'https://rafaelpaulene-5twa.onrender.com';
+$config['base_url'] 				= '';
 
 /*
 |--------------------------------------------------------------------------
