ac8d6afe2600b331a3e3ec941d7d062317c86b6f
Paulene Rafael <paupau73@gmail.com>
2025-09-22 15:07:06 +0800

    first commit

--- app/config/database.php
+++ app/config/database.php
@@ -62,9 +62,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12799154',
-    'password'	=> 'TJPEC4wlYx',
-    'database'	=> 'sql12799154',
+    'username'	=> 'sql12799094',
+    'password'	=> 'ypndpGlzKT',
+    'database'	=> 'sql12799094',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
